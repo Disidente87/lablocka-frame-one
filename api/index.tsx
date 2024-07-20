880640b57bdf7838c58c7576f4beac0291da09a3
Disidente87 <criptodisidente@gmail.com>
2024-07-20 10:28:37 -0700

    deploy to vercel

--- api/index.tsx
+++ api/index.tsx
@@ -1,7 +1,8 @@
-import { Button, Frog, TextInput } from 'frog'
+import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
+import { pinata } from 'frog/hubs'
 import { serveStatic } from 'frog/serve-static'
-// import { neynar } from 'frog/hubs'
+//import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
 
 // Uncomment to use Edge Runtime.
@@ -12,59 +13,94 @@ import { handle } from 'frog/vercel'
 export const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
+  hub: pinata(),
+  title: 'La BLocka',
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 })
 
-app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+app.frame('/main', (c) => {
+  const {  } = c
+
   return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
-    ),
+    image: "/images/1.jpg",
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      //<TextInput placeholder="Enter cu" />,
+      <Button action='/error'>Manolo</Button>,
+      <Button action='/error'>Fifardo</Button>,
+      <Button action='/work'>Arby</Button>,
+      <Button action='/error'>Soccero</Button>,
+      //status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   })
 })
+
+
+app.frame('/error', (c) => {
+  const {  } = c
+
+  return c.res({
+    image: "/images/5.jpg",
+    intents: [
+      //<TextInput placeholder="Enter cu" />,
+      <Button action="/main">Volver a Intentar</Button>,
+    
+      //status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+app.frame('/work', (c) => {
+  const {  } = c
+
+  return c.res({
+    image: "/images/2.jpg",
+    intents: [
+      //<TextInput placeholder="Enter cu" />,
+      <Button action='/error'>Polkadot</Button>,
+      <Button action='/error'>Ethereum</Button>,
+      <Button action='/error'>Base</Button>,
+      <Button action='/buy'>Arbitrum</Button>,
+    
+      //status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+app.frame('/buy', (c) => {
+  const {  } = c
+
+  return c.res({
+    image: "/images/3.jpg",
+    intents: [
+      //<TextInput placeholder="Enter cu" />,
+      <Button action='/error'>con la tía piolín</Button>,
+      <Button action='/error'>Con Shillardo Ponzi</Button>,
+      <Button action='/last'>En Bando.cool</Button>,
+      <Button action='/error'>En la tienda de la esquina</Button>,
+    
+      //status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+app.frame('/last', (c) => {
+  const {  } = c
+
+  return c.res({
+    image: "/images/4.jpg",
+    intents: [
+      //<TextInput placeholder="Enter cu" />,
+      <Button.Link href='https://giveth.io/es/project/la-blocka-animated-episode-of-arbitrum-in-la-blocka?apcid=00652c2c0f7f9fb81c6adc00' >Giveth</Button.Link>,
+      <Button.Link href='https://zora.co/collect/arb:0xf38ce72d52302d5ae99ba1d4e96fe22a38fd8d58/1?referrer=0x800791E9Ff96fFeA46AB13633Ad58EC6c43c5421'>Free Mint</Button.Link>,
+      <Button.Link href='https://warpcast.com/lablockatoon'>La Blocka</Button.Link>,
+      <Button.Link href='https://warpcast.com/lablockatoon'>Share</Button.Link>,
+    
+      //status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
 
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== 'undefined'
